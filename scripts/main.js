13775550836daee6f50df83f9b8b45b9b8e743fa
Wilson Neto <wilson.neto@encripta.com.br>
2020-04-29 00:22:20 -0300

    configuring routes

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 $(function () {
   console.log('ok...');
-  ko.applyBindings(WebmailViewModel);
+  ko.applyBindings(new WebmailViewModel());
 });
--- scripts/view_models/WebmailViewModel.js
+++ scripts/view_models/WebmailViewModel.js
@@ -1,11 +1,26 @@
 function WebmailViewModel() {
   // Data
   var self = this;
-  self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
-  self.chosenFolderId = ko.observable();
+
+  self.isLoading = ko.observable(false);
+  self.moviesList = ko.observable();
+  self.movieItem = ko.observable();
 
   // Behaviours
-  self.goToFolder = function (folder) {
-    self.chosenFolderId(folder);
+  self.goToDetail = function (item) {
+    document.location.hash = item.id;
   };
+
+  $.get('http://movies-example.will.tmp.br/movies', null, self.moviesList);
+
+  // Client-side routes
+  Sammy(function () {
+    this.get('#:movieId', function () {
+      $.get(
+        'http://movies-example.will.tmp.br/movies/' + this.params.movieId,
+        null,
+        self.movieItem
+      );
+    });
+  }).run();
 }
